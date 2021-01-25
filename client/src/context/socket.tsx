aedf43bdeb80aa658952a2af03e39e84887da900
Obi chinedu frank <frankieetchy@gmail.com>
2021-01-25 08:31:44 +0100

    add socket context

---
+++ client/src/context/socket.tsx
@@ -0,0 +1,35 @@
+import React, { useState, createContext, useEffect } from 'react';
+import socketClient from 'socket.io-client';
+
+interface ISocketContext {
+  socket: SocketIOClient.Socket | undefined
+}
+
+const SocketContext = createContext<ISocketContext| undefined>(undefined);
+const SOCKET_SERVER_URL = 'http://127.0.0.1:3002';
+console.log('SOCKET_SERVER_URL', SOCKET_SERVER_URL);
+
+const io: SocketIOClient.Socket = socketClient.connect(SOCKET_SERVER_URL, {
+  transports: [ 'websocket', 'polling']
+});
+
+const SocketProvider = (props: any) => {
+  const [socket, setSocket] = useState<SocketIOClient.Socket>();
+  useEffect(() => {
+    if(!socket) {
+      io.on('connect', () => {
+        console.log('wahala be like bycycle');
+      });
+      setSocket(io);
+    }
+  }, [socket])
+
+    return (
+        <SocketContext.Provider
+            value={{ socket }}
+        >
+            {props.children}
+        </SocketContext.Provider>
+    );
+}
+export { SocketContext, SocketProvider };
--- client/src/views/App.tsx
+++ client/src/views/App.tsx
@@ -2,22 +2,25 @@ import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import { Switch } from 'react-router-dom';
 import { routes } from '../routes';
+import {SocketContext, SocketProvider} from '../context/socket'
 
 const App: React.FC= () =>  {
 	const [id] = useState(nanoid);
 
   return (
 	<div className=" bg-black min-h-screen">
-		<Switch>
-		{routes.map(({path, component, RouteType}) => (
-			<RouteType
-			key={id}
-			path={path}
-			exact
-			component={component}
-		/>
-		))}
-		</Switch>
+    <SocketProvider>
+      <Switch>
+      {routes.map(({path, component, RouteType}) => (
+        <RouteType
+        key={id}
+        path={path}
+        exact
+        component={component}
+      />
+      ))}
+      </Switch>
+    </SocketProvider>
 	</div>
 	)
 }
