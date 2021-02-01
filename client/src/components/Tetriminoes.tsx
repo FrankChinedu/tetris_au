ae28c49ad99c4581321191f0767978d3b477d7c9
Elizabeth Lola <leezeelola@gmail.com>
2021-02-01 17:17:52 +0100

    redesigned gameover popup and seperated tetrimomnes

---
+++ client/src/components/Tetriminoes.tsx
@@ -0,0 +1,35 @@
+export const TShape = () => (
+    <div className="mt-1 w-20 transform -rotate-45">
+        <div className="flex mt-1">
+            <div className="h-6 w-6 mr-1 rounded bg-pink-700"></div>
+            <div className="h-6 w-6 mr-1 rounded bg-pink-700"></div>
+            <div className="h-6 w-6 mr-1 rounded bg-pink-700"></div>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+            <div className="h-6 w-6 mr-1 rounded bg-pink-700 my-1"></div>
+        </div>
+    </div>
+);
+
+export const OShape = () => (
+    <div className="flex transform">
+        <div className="grid gap-1">
+            <div className="h-6 w-6 mr-1 rounded bg-purple-700"></div>
+            <div className="h-6 w-6 mr-1 rounded bg-purple-700"></div>
+        </div>
+        <div className="grid gap-1">
+            <div className="h-6 w-6 mr-1 rounded bg-purple-700"></div>
+            <div className="h-6 w-6 mr-1 rounded bg-purple-700"></div>
+        </div>
+    </div>
+);
+
+export const IShape = () => (
+    <div className="flex transform">
+        <div className="h-6 w-6 mr-1 rounded bg-blue-500"></div>
+        <div className="h-6 w-6 mr-1 rounded bg-blue-500"></div>
+        <div className="h-6 w-6 mr-1 rounded bg-blue-500"></div>
+        <div className="h-6 w-6 mr-1 rounded bg-blue-500"></div>
+    </div>
+);
+
