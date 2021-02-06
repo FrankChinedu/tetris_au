187bb440af4bf14692f38ab34c361845cf53daec
Elizabeth Lola <leezeelola@gmail.com>
2021-02-06 20:04:45 +0100

    redesigned how to play tetris

--- client/src/views/HowToPlay/index.tsx
+++ client/src/views/HowToPlay/index.tsx
@@ -25,49 +25,16 @@ const MultiPlayerSteps: React.FC = () => {
         <h3 className="md:text-5xl text-lg col-span-2 mr-auto p-3">How to play Tetris</h3>
       </nav>
       <main className="md:max-w-3xl w-11/12 mx-auto px-1">
-          {width && width < 500 ? (
-            <div>
-              <details className="my-10">
-                <summary>How to play single Player</summary>
-                <p className="montserrat my-10">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore beatae reprehenderit cupiditate impedit, iure minus nulla nam excepturi, nihil quo cumque aspernatur, nemo ipsam neque? Laboriosam perferendis consectetur eligendi sequi.</p>
-              </details>
-              <details className="my-10">
-                <summary>How to play single Player</summary>
-                <p className="montserrat my-10">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore beatae reprehenderit cupiditate impedit, iure minus nulla nam excepturi, nihil quo cumque aspernatur, nemo ipsam neque? Laboriosam perferendis consectetur eligendi sequi.</p>
-              </details>
-            </div>
-          ) : (
-            <ul className="overflow-hidden h-100">
-              <li>
-                <p
-                  className="block h-100 w-20 float-left overflow-hidden mb-10 bg-gray-900 text-center transition-all"
-                  onClick={() => {
-                    checked2 && setChecked2(false)
-                    setChecked(!checked)
-                  }}
-                >
-                  Single Player</p>
-                <div className={`montserrat block w-0 px-3 float-left overflow-hidden opacity-0 transition-all ${checked && ('w-72 opacity-100')}`}>
-                  <h2>Slide 1</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur beatae voluptate neque inventore in nesciunt molestias eos soluta nulla explicabo?</p>
-                </div>
-              </li>
-              <li>
-                <p
-                  className="block h-100 w-20 float-left overflow-hidden mb-10 bg-gray-900 text-center transition-all"
-                  onClick={() => {
-                    checked && setChecked(false)
-                    setChecked2(!checked2)
-                  }}
-                >
-                  Multi Player</p>
-                <div className={`montserrat block w-0 px-3 float-left overflow-hidden opacity-0 transition-all ${checked2 && (' w-72 opacity-100')}`}>
-                  <h2>Slide 1</h2>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis vitae nisi provident nobis dignissimos perferendis voluptate sint, corporis aliquam tenetur quia, obcaecati pariatur. Corporis totam debitis maiores magni aperiam minus.</p>
-                </div>
-              </li>
-            </ul>
-          )}
+        <div>
+          <details className="my-10">
+            <summary className="text-3xl p-2">How to play single Player</summary>
+            <p className="montserrat my-10">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore beatae reprehenderit cupiditate impedit, iure minus nulla nam excepturi, nihil quo cumque aspernatur, nemo ipsam neque? Laboriosam perferendis consectetur eligendi sequi.</p>
+          </details>
+          <details className="my-10">
+            <summary className="text-3xl p-2">How to play multi Player</summary>
+            <p className="montserrat my-10">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore beatae reprehenderit cupiditate impedit, iure minus nulla nam excepturi, nihil quo cumque aspernatur, nemo ipsam neque? Laboriosam perferendis consectetur eligendi sequi.</p>
+          </details>
+        </div>
       </main>
     </div>
   );
