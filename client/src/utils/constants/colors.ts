77b465921c4e7d454cdfe56269e0129d62c004d7
Elizabeth Lola <leezeelola@gmail.com>
2021-02-08 20:27:09 +0100

    temp design for leaderboard

---
+++ client/src/utils/constants/colors.ts
@@ -0,0 +1,15 @@
+const COLORS = [
+    'bg-red-300',
+    'bg-blue-400',
+    'bg-green-500',
+    'bg-pink-300',
+    'bg-purple-500',
+    'bg-yellow-400',
+    'bg-indigo-500',
+    'bg-pink-500',
+    'bg-yellow-300',
+    'bg-red-500',
+    'bg-blue-200',
+]
+
+export default COLORS;
