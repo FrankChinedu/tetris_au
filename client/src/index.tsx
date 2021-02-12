31ecff883f692530ba23a152dba622d2a3c2c875
Elizabeth Lola <leezeelola@gmail.com>
2021-02-12 19:51:24 +0100

    google analytics works

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
 
 import './index.css';
 import App from './views/App';
@@ -10,22 +9,14 @@ import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
-ReactGA.initialize("UA-189671562-1");
+ReactGA.initialize("UA-189671562-1", {debug: true});
 
-const history = createBrowserHistory();
-
-history.listen(location => {
-    ReactGA.set({ page: location.pathname }); // Update the user's current page
-    ReactGA.pageview(location.pathname); // Record a pageview for the given page
-});
 
 ReactDOM.render(
   <React.StrictMode>
-      <Router history={history}>
-        <BrowserRouter>
-        <App />
-        </BrowserRouter>
-      </Router>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
