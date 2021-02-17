38f8beff51b1e60b1fe309a282db840195155b4d
Obi chinedu frank <frankieetchy@gmail.com>
2021-02-17 15:12:00 +0100

    add twitter auth

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import { json } from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import passport from 'passport';
+import session from 'express-session';
 import { Socket, Server as SocketServer } from 'socket.io';
 
 import Route from './route';
@@ -25,8 +25,15 @@ class Server {
       res.send('You have reached the API!');
     });
 
-    this.app.use(passport.initialize());
-    this.app.use(passport.session());
+    this.app.use(session({
+      secret: ' secret',
+      cookie: {
+        secure: false,
+        maxAge: 269999999999
+      },
+      saveUninitialized: true,
+      resave: true
+    }));
     this.route(this.app);
 
     this.app.use(express.static(`${path.resolve('./')}/build/client`));
--- server/route/twitter.ts
+++ server/route/twitter.ts
@@ -1,18 +1,36 @@
-import express from 'express';
-import passport from '../middleware/passport';
-// import TwitterController from '../module/twitter';
+import express, { Request, Response } from 'express';
+import tw from '../middleware/twitter';
 
 const Router = express.Router();
 
-Router.get('/twitter', passport.authenticate('twitter', { scope: ['include_email=true'] }));
-// Router.get('/twitter', (req, res) => {
-//   res.send('some');
-// });
+Router.get('/twitter', (req: Request, res: Response) => {
+  tw.login((err: Error, tokenSecret: string, url: string) => {
+    if (err) {
+      // Handle the error your way
+      console.log('error', err);
+    }
+    console.log(tokenSecret, 'tokensecret');
+    console.log(url, 'url');
 
-Router.get('/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/auth/error' }),
-  function (req, res) {
-    res.redirect('/game/end');
+    // Save the OAuth token secret for use in your /twitter/callback route
+    req.session.tokenSecret = tokenSecret;
+    res.redirect(url);
   });
-// Router.get('/callback', TwitterController.log);
+});
+
+Router.get('/twitter/callback', (req: Request, res: Response) => {
+  tw.callback({
+    oauth_token: req.query.oauth_token,
+    oauth_verifier: req.query.oauth_verifier
+  }, req.session.tokenSecret, (err: Error, user: any) => {
+    if (err) {
+      // Handle the error your way
+      console.log('callback', err);
+    }
+    console.log('user allback', user);
+
+    res.redirect('/');
+  });
+});
 
 export default Router;
