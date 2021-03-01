52ee690267fc9b71942ec40c1b7732c4831c9cec
Elizabeth Lola <leezeelola@gmail.com>
2021-03-01 23:22:34 +0100

    game setting UI completed

--- client/src/views/Settings/index.tsx
+++ client/src/views/Settings/index.tsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactGA from 'react-ga';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, } from '@fortawesome/free-solid-svg-icons';
+import './settings.css';
 
 
 import ROUTES from '../../utils/constants/routes';
 
 
 const Settings: React.FC = () => {
+
+  const [range, setRange] = useState<number | string>(1000);
+  const [username, setUsername] = useState<string>("");
 
   useEffect(() => {
     ReactGA.pageview(window.location.pathname + window.location.search);
@@ -23,8 +27,29 @@ const Settings: React.FC = () => {
         <h3 className="md:text-5xl text-lg col-span-2 mr-auto p-3">Settings</h3>
       </nav>
       <main className="md:max-w-3xl w-11/12 mx-auto px-1 montserrat">
-        <div className="flex justify-center items-center">
-            different settings options here
+        <div className="">
+          <section className="grid grid-cols-2 mb-5">
+            <p className="text-xl">Game Sound</p>
+            <p>
+              <input type="checkbox" id="switch" className="invisible w-0 h-0" />
+              <label htmlFor="switch" className="cursor-pointer w-24 h-12 bg-gray-500 block rounded-full relative -top-6" />
+            </p>
+          </section>
+          <section className="grid grid-cols-2 mb-5">
+            <p className="text-xl">Game Speed</p>
+            <div>
+              <input type="range" min="1000" max="10000" value={range} onChange={(e) => setRange(e.target.value)} className="appearance-none w-full h-3 rounded-md outline-none bg-gray-500 opacity-70 transition-opacity duration-1000 hover:opacity-100" />
+              <p>Value: {range}</p>
+            </div>
+            <small>(Only for Single player)</small>
+          </section>
+          <section className="grid grid-cols-2 mb-5">
+            <p className="text-xl">Change Twitter Username</p>
+            <div>
+              <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="focus:outline-none bg-black border border-red-300 p-2 block w-full mb-1" />
+              <small>don't add @ just add your username, example ilizette7</small>
+            </div>
+          </section>
         </div>
       </main>
     </div>
