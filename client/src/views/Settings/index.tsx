3c0f65edc2641b4a1fa23339369bdef30e6011f8
Elizabeth Lola <leezeelola@gmail.com>
2021-03-01 23:28:55 +0100

    removed unused codes

--- client/src/views/Settings/index.tsx
+++ client/src/views/Settings/index.tsx
@@ -26,31 +26,29 @@ const Settings: React.FC = () => {
         </Link>
         <h3 className="md:text-5xl text-lg col-span-2 mr-auto p-3">Settings</h3>
       </nav>
-      <main className="md:max-w-3xl w-11/12 mx-auto px-1 montserrat">
-        <div className="">
-          <section className="grid grid-cols-2 mb-5">
-            <p className="text-xl">Game Sound</p>
-            <p>
-              <input type="checkbox" id="switch" className="invisible w-0 h-0" />
-              <label htmlFor="switch" className="cursor-pointer w-24 h-12 bg-gray-500 block rounded-full relative -top-6" />
-            </p>
-          </section>
-          <section className="grid grid-cols-2 mb-5">
-            <p className="text-xl">Game Speed</p>
-            <div>
-              <input type="range" min="1000" max="10000" value={range} onChange={(e) => setRange(e.target.value)} className="appearance-none w-full h-3 rounded-md outline-none bg-gray-500 opacity-70 transition-opacity duration-1000 hover:opacity-100" />
-              <p>Value: {range}</p>
-            </div>
-            <small>(Only for Single player)</small>
-          </section>
-          <section className="grid grid-cols-2 mb-5">
-            <p className="text-xl">Change Twitter Username</p>
-            <div>
-              <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="focus:outline-none bg-black border border-red-300 p-2 block w-full mb-1" />
-              <small>don't add @ just add your username, example ilizette7</small>
-            </div>
-          </section>
-        </div>
+      <main className="md:max-w-3xl w-11/12 mx-auto px-1 montserrat mt-5">
+        <section className="grid grid-cols-2 mb-5">
+          <p className="sm:text-xl ">Game Sound</p>
+          <p>
+            <input type="checkbox" id="switch" className="invisible w-0 h-0" />
+            <label htmlFor="switch" className="cursor-pointer w-24 h-12 bg-gray-500 block rounded-full relative -top-6" />
+          </p>
+        </section>
+        <section className="grid grid-cols-2 mb-5">
+          <p className="sm:text-xl ">Game Speed</p>
+          <div>
+            <input type="range" min="1000" max="10000" value={range} onChange={(e) => setRange(e.target.value)} className="appearance-none w-full h-3 rounded-md outline-none bg-gray-500 opacity-70 transition-opacity duration-1000 hover:opacity-100" />
+            <p>Value: {range}</p>
+          </div>
+          <small>(Only for Single player)</small>
+        </section>
+        <section className="grid grid-cols-2 mb-5">
+          <p className="sm:text-xl ">Change Twitter Username</p>
+          <div>
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="focus:outline-none focus:border-opacity-100 border-opacity-50 transition-all bg-black border border-red-300 p-2 block w-full mb-1" />
+            <small>don't add @ just add your username, example ilizette7</small>
+          </div>
+        </section>
       </main>
     </div>
   );
